0dde29a79fb13cdfd9bc74fa8803c036833989e4
David Lavanchy <dplavanchy@gmail.com>
2021-10-14 17:59:03 -0400

    built out Category Data and Models Layer

---
+++ ElevenNote.Models/CategoryModels/CategoryCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models.CategoryModels
+{
+    public class CategoryCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(50, ErrorMessage = "There are too many characters in this field. Title must be less than or equal to 50 characters")]
+        public string Title { get; set; }
+        [MaxLength(300)]
+        public string Description { get; set; }
+    }
+}
---
+++ ElevenNote.Models/CategoryModels/CategoryListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models.CategoryModels
+{
+    public class CategoryListItem
+    {
+        public int CategoryId { get; set; }
+        [DisplayName("Title")]
+        public string Title { get; set; }
+    }
+}
