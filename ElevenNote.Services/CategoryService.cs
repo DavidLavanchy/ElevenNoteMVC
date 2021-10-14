49104ced9fd23059cc34c60e84b4a90c22806749
David Lavanchy <dplavanchy@gmail.com>
2021-10-14 18:04:28 -0400

    Scaffolded out Category Controller and Services layer

---
+++ ElevenNote.Services/CategoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class CategoryService
+    {
+        private readonly Guid _userId;
+        public CategoryService(Guid userId)
+        {
+            _userId = userId;
+        }
+    }
+}
