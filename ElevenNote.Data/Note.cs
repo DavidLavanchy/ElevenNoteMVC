6a1da6f698fb521762cc59689ed89ad595b1bdf8
David Lavanchy <dplavanchy@gmail.com>
2021-10-18 11:52:57 -0400

    added the star property in Note class and migrated to database

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -25,5 +26,8 @@ namespace ElevenNote.Data
         [ForeignKey(nameof(Category))]
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsStarred { get; set; }
     }
 }
